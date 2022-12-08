cfd436a81ba71d1d0f0666022aeeef9766b1cf73
Ryan Faulhaber <ryf@sent.as>
2022-12-07 20:17:39 -0500

    remove code

--- 6/main.hs
+++ 6/main.hs
@@ -11,13 +11,6 @@ windowStr :: Int -> String -> [String]
 windowStr _    ""  = []
 windowStr size str = [take size str] ++ (windowStr size (drop 1 str))
 
-solvePart1 :: String -> Int
-solvePart1 = (+ 4) . inner 0 . windowStr 4
- where
-  inner :: Int -> [String] -> Int
-  inner n (x : xs) = if distinct 4 x then n else inner (n + 1) xs
-  inner _ []       = 0
-
 solveN :: Int -> String -> Int
 solveN size = (+ size) . inner 0 . windowStr size
  where
