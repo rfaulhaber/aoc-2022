7d551754334cc9519a785f98304dd79258de278c
Ryan Faulhaber <ryf@sent.as>
2022-12-15 16:57:42 -0500

    more day 8 work

--- 8/main.hs
+++ 8/main.hs
@@ -9,6 +9,8 @@ import           Text.Printf
 type Grid = [[Integer]]
 type GridPos = (Integer, Integer, Integer)
 
+data Direction = Up | Down | Left | Right deriving (Eq, Show)
+
 getGrid :: String -> Grid
 getGrid = map (map read) . map (tail . splitOn "") . words
 
@@ -36,27 +38,52 @@ getAdjacent grid (x, y) =
       , getElementInGrid (x, y + 1)
       )
 
+isEdge :: Grid -> (Integer, Integer) -> Bool
+isEdge _ (_, 0) = True
+isEdge _ (0, _) = True
+isEdge grid (x, y) =
+  let (xmax, ymax) = getGridDimensions grid
+  in  x == (xmax - 1) || y == (ymax - 1)
+
+getAdjacentByDirection :: Grid -> Direction -> (Integer, Integer) -> Integer
+getAdjacentByDirection grid direction pos =
+  getElement grid $ getPos direction pos
+
+getPos :: Direction -> (Integer, Integer) -> (Integer, Integer)
+getPos Main.Up    (x, y) = (x, y - 1)
+getPos Main.Down  (x, y) = (x, y + 1)
+getPos Main.Left  (x, y) = (x - 1, y)
+getPos Main.Right (x, y) = (x + 1, y)
+
+getPathToEdge :: Grid -> Direction -> (Integer, Integer) -> [Integer]
+getPathToEdge grid direction pos =
+  map (getElement grid) . takeWhile (not . (isEdge grid)) $ iterate
+    (getPos direction)
+    pos
+
+-- isVisible :: Grid -> GridPos -> Bool
+-- isVisible _ (0, _, _) = True
+-- isVisible _ (_, 0, _) = True
+-- isVisible grid (x, y, _)
+--   | x
+--     == fromIntegral (length (head grid))
+--     -  1
+--     || y
+--     == fromIntegral (length grid)
+--     -  1
+--   = True
+-- isVisible grid (x, y, p) =
+--   let (back, top, right, down) = getAdjacent grid (x, y)
+--   in  p > back && p > top && p > right && p > down
+
 isVisible :: Grid -> GridPos -> Bool
-isVisible _ (0, _, _) = True
-isVisible _ (_, 0, _) = True
-isVisible grid (x, y, _)
-  | x
-    == fromIntegral (length (head grid))
-    -  1
-    || y
-    == fromIntegral (length grid)
-    -  1
-  = True
+isVisible grid (x, y, _) | isEdge grid (x, y) = True
 isVisible grid (x, y, p) =
-  let (back, top, right, down) = getAdjacent grid (x, y)
-  in  (trace ("vals " ++ show (back, top, right, down) ++ " " ++ show p) p)
-        >  back
-        && p
-        >  top
-        && p
-        >  right
-        && p
-        >  down
+  let edgePathFns =
+        map (getPathToEdge grid) $ [Main.Up, Main.Down, Main.Left, Main.Right]
+      edgePaths = map (\fn -> fn (x, y)) edgePathFns
+  in  foldl (\acc n -> acc && n) True edgePaths
+
 
 solvePart1 :: String -> Integer
 solvePart1 input =
@@ -64,7 +91,7 @@ solvePart1 input =
       (xmax, ymax) = getGridDimensions grid
       indices      = [ (x, y) | x <- [0 .. xmax - 1], y <- [0 .. ymax - 1] ]
       elements     = map (\(x, y) -> (x, y, getElement grid (x, y))) indices
-  in  fromIntegral . length . filter (== True) . map (isVisible grid) $ elements
+  in  fromIntegral . length . filter id . map (isVisible grid) $ elements
 
 solvePart2 :: String -> Integer
 solvePart2 = undefined
